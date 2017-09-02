9f21afcdef6ec2a6e30959ae1eefb0d64826e0f8
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-09-02 16:26:41 +0800

    Implementing AuthGuards for lazy routes

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -5,16 +5,17 @@ import { ScheduleModule } from './schedule/schedule.module';
 import { MealsModule } from './meals/meals.module';
 import { WorkoutsModule } from './workouts/workouts.module';
 
+import { AuthGuard } from './../auth/shared/guards/auth.guard';
+
 export const ROUTES: Routes = [
-    { path: 'schedule', loadChildren: './schedule/schedule.module#ScheduleModule' },
-    { path: 'meals', loadChildren: './meals/meals.module#MealsModule' },
-    { path: 'workouts', loadChildren: './workouts/workouts.module#WorkoutsModule' }
+    { path: 'schedule',canActivate: [ AuthGuard ], loadChildren: './schedule/schedule.module#ScheduleModule' },
+    { path: 'meals', canActivate: [ AuthGuard ], loadChildren: './meals/meals.module#MealsModule' },
+    { path: 'workouts', canActivate: [ AuthGuard ], loadChildren: './workouts/workouts.module#WorkoutsModule' }
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forChild(ROUTES),
-        MealsModule
+        RouterModule.forChild(ROUTES)
     ]
 })
 export class HealthModule {}
