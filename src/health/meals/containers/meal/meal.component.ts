b8954f907121ac4efc32f0c9e7c883271b8a54ee
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-09-02 18:03:42 +0800

    Async Pipe as syntax and routing

---
+++ src/health/meals/containers/meal/meal.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meal',
+    styleUrls: ['meal.component.scss'],
+    template: `
+        <div class="meal">
+          I'm a meal
+            
+        </div>
+    `
+})
+export class MealComponent {
+    constructor() {}
+}
